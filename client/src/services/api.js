cb9f38e94eb592056d653c0701635222e5ad1618
zubaircsexpert <ranazubairmunawar@gmail.com>
2026-04-19 13:52:19 +0500

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
 
-baseURL: "https://proctorsecure-ai-jkc2.onrender.com"
+baseURL: "https://proctorsecure-ai-jkc2.onrender/api.com"
 });
 
 API.interceptors.request.use((req) => {
