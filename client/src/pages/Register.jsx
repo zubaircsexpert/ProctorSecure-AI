a45d3c63bdfce1ed7ed937a3d583d00d4536d550
zubaircsexpert <ranazubairmunawar@gmail.com>
2026-04-19 13:50:10 +0500

    Update Register.jsx

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -12,7 +12,7 @@ function Register() {
   const navigate = useNavigate();
 
   // .env se URL le raha hai, agar nahi milega toh default localhost use karega
-  const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+  const API_BASE_URL = import.meta.env.VITE_API_URL || "https://proctorsecure-ai-jkc2.onrender.com/api";
 
   const handleRegister = async (e) => {
     e.preventDefault();
