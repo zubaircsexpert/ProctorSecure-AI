4384e09955b4c6730672a2999dfe164d10372c76
Muhammad Zubair <ranazubairrana667@gmail.com>
2026-04-23 19:57:57 +0500

    first-commit

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://proctorsecure-ai-jkc2.onrender.com/api",
+  baseURL: "https://proctorsecure-ai-jkc2.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
